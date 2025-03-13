151a10008cfa188a09b45fc00b5ddab782f7520c
faythr328 <fmrackeweg0926@icloud.com>
2025-03-12 20:19:18 -0400

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import './App.css';
 import LoginPage from './pages/LoginPage.tsx';
-// import CardPage from './pages/CardPage.tsx';
-//import React from 'react';
 import { BrowserRouter as Router, Route, Navigate, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
+      <div>
+       <h1>Welcome to YourUCF</h1>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
+      </div>
     </Router>
   );
 }
