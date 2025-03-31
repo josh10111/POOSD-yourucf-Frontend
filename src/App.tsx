a77db70a4829f4d22e157e29ea5166e174a5b6d6
faythr328 <fmrackeweg0926@icloud.com>
2025-03-31 16:34:28 -0400

    added verify

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import './App.css';
 import LoginPage from './pages/LoginPage.tsx';
 import Dashboard from './pages/DashPage.tsx';
+import VerifyPage from './pages/VerifyPage.tsx';
 import { BrowserRouter as Router, Route, Navigate, Routes } from 'react-router-dom';
 
-function App() {
+function App() 
+{
   return (
     <Router>
       <div>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/verify" element={<VerifyPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
@@ -18,3 +21,4 @@ function App() {
 }
 
 export default App;
+
